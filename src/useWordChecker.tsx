f9c43ea959cb9cc7f215230dc1ffbfb50d96cc53
Shakti <shakticse@gmail.com>
2022-02-21 08:38:02 -0600

    Update useWordChecker.tsx

--- src/useWordChecker.tsx
+++ src/useWordChecker.tsx
@@ -1,5 +1,19 @@
-export default function userWordChecker() {
-   const {word, setWord} = useState('');
-  
-  return {word, setWord};
+import { useCallback, useState } from "react";
+const wordLength = 5;
+export default function useWordChecker(wordChecked) {
+  const { word, setWord } = useState(wordChecked);
+  const wordMatchingArray = new Array(wordLength).fill(0);
+
+  const checkWord = useCallback((wordToCheck) => {
+    for (let i = 0; i < wordLength; i++) {
+      if (word[i] === wordToCheck[i]) {
+        wordMatchingArray[i] = 1;
+      } else if (false) {
+        // do something for if letter is present in word
+      }
+    }
+    return wordMatchingArray;
+  });
+
+  return { wordMatchingArray };
 }
