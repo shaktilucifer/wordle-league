2c1053f9608509d8390ded77409d204ee44be65f
Shakti <shakticse@gmail.com>
2022-02-24 23:34:16 -0500

    make board look good

--- src/Board.js
+++ src/Board.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import WordRow from './WordRow';
 import { useEffect, useState } from 'react';
+import './Board.css';
 
 export default function Board() {
 
