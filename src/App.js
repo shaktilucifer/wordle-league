79c891d7071ef0c770daba35f2e64ab9626cc02b
Shakti <shakticse@gmail.com>
2022-03-21 23:56:33 -0400

    minor fix

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
 
   const onClickKey = useCallback((clickedLetter) => {
     if(clickedLetter === ENTER_KEY) {
-
       setCurrentRow(prevRow => {
         wordMap[currentRow] = currentWord;
         setCurrentWord('');
