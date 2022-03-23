474f2d28aeddc0a985c45f68ca595c1e750db998
Shakti <shakticse@gmail.com>
2022-03-22 22:40:04 -0400

    check wordmap fix

--- src/Board.js
+++ src/Board.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import WordRow from './WordRow';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './Board.css';
 
 export default function Board({wordMap, clickedLetter, currentWord, currentRow}) {
 
   const [word, setWord] = useState('');
+
+  const getCharacterArray = useCallback((index) {
+    return currentRow === index ? currentWord.split('') : wordMap[index] ? wordMap[index].split('') : []
+  }, [currentRow, currentWord, wordMap]);
   
   return (
     <div className="Board">
      { [...Array(5)].map((index) => {
-        return <WordRow key={'row_'+index} 
-        letterArray={currentRow === index ? currentWord.split('') : wordMap[index] ? wordMap[index].split('') : []} />
+        return <WordRow key={'row_'+index} letterArray={getCharacterArray(index)} />
       })}
     </div>
   );
